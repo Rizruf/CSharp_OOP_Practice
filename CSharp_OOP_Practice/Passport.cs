91d8be0fbbb72b4fec2612ace1406a8093614a6a
Rizruf <sova_nikeo@mail.ru>
2025-12-23 15:36:19 +0600

    Особенности: Введение в ООП и подготовительная задача на создание паспорта с private и инкапсуляцией.

---
+++ CSharp_OOP_Practice/Passport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_OOP_Practice
+{
+    internal class Passport
+    {
+        private string FIO;
+        private string Number;
+        private DateOnly IssueDate;
+
+        public Passport(string fio, string number, DateOnly data)
+        {
+            FIO = fio;
+            Number = number;
+            IssueDate = data;
+        }
+
+        public void Print_Info()
+        {
+            Console.WriteLine($"{FIO} || {Number} || {IssueDate}");
+        }
+    }
+}
---
+++ CSharp_OOP_Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp_OOP_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Passport p = new Passport("Асуханов Игорь Юрьевич", "5200 123456", new DateOnly(2022, 05, 20));
+
+            p.Print_Info();
+        }
+    }
+}
